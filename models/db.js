8215122b5f16d14d2370c41c66a129d039bde139
La'Tonia Mertica <latoniamertica@gmail.com>
2021-08-06 00:44:06 -0400

    drafting models relationships

--- models/db.js
+++ models/db.js
@@ -18,15 +18,36 @@ const connectToDB = async () => {
   await db.authenticate();
   console.log(`Database Connected Successfully`);
 
-  Customers.hasMany(Favorites, { foreignKey: "customerID" });
-  Favorites.belongsTo(Creatives, { foreignKey: "creativeID" });
   Customers.belongsTo(Users, { foreignKey: "userID" });
+  Customers.hasMany(Favorites, { foreignKey: "customerID" });
+  Customers.hasMany(CreativesInCart, { foreignKey: "cartID" });
+
+  Cart.belongsTo(Customers, { foreignKey: "customerID" });
+  Cart.hasMany(CreativesInCart, { foreignKey: "cartID" });
+  Favorites.belongsTo(Creatives, { foreignKey: "creativeID" });
+
+  Creatives.hasMany(Creatives, { primaryKey: "creativeID" });
 
   await db.sync(); //{ force: true }
 
   const customers = await Customers.findAll();
   if (customers.length === 0) {
     await Customers.create({ nameFirst: "Testy", nameLast: "McTesterson" });
+  }
+  const favorites = await Favorites.findAll();
+  if (favorites.length === 0) {
+    await Favorites.create({ customerID: 1, creativeID: 1 });
+  }
+  const creativesInCart = await CreativesInCart.findAll();
+  if (CreativesInCart.length === 0) {
+    await CreativesInCart.create({
+      customerID: 1,
+      cartID: 1,
+    });
+  }
+  const cart = await Cart.findAll();
+  if (Cart.length === 0) {
+    await Cart.create({ customerID: 1 });
   }
 
   const creatives = await Creatives.findAll();
